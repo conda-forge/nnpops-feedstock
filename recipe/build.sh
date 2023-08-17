87976ff8c2f31db37b0c105df30b17317b01abce
RaulPPealez <raulppelaez@gmail.com>
2023-08-17 08:27:31 +0200

    Update comments

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,8 +13,8 @@ CMAKE_FLAGS+=" -DTorch_DIR=${SP_DIR}/torch/share/cmake/Torch"
 declare -a CUDA_CONFIG_ARGS
 if [ ${cuda_compiler_version} != "None" ]; then
     # Output format from torch._C._cuda_getArchFlags():
-    # ['sm_37', 'sm_50', 'sm_60', 'sm_70', 'sm_75', 'sm_80', 'sm_86', 'compute_86', 'sm_90']
-    # We need to turn this into: "3.7;5.0;6.0;7.0;7.5;8.0;8.6;9.0" for CMake
+    # ['sm_35 sm_50 sm_60 sm_61 sm_70 sm_75 sm_80 sm_86 compute_86']
+    # We need to turn this into: "3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6" for pytorch in CMake
     # There is a higher level function, called torch.cuda.get_arch_list, but it returns an empty list when there is no GPU available.
     # Should that fail, this could be used instead:
     # $ cuobjdump $(find $CONDA_PREFIX -name "libtorch_cuda.so")  | grep arch | awk '{print $3}' | sort | uniq | sed 's+sm_\([0-9]\)\([0-9]\)+\1.\2+g' | tr '\n' ';'
